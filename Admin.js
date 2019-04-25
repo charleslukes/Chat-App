93410300cd35f718ea23179ca57e04110d222cd1
Charles Chiakwa <charleslukes28@gmail.com>
2019-04-25 15:20:22 +0100

    Admin inherits from user constructor

--- Admin.js
+++ Admin.js
@@ -6,5 +6,11 @@ function Admin(name, password){
     DB.admin.push(this);
 }
 
+//admin inherit from the user function
+Admin.prototype = Object.create(user.prototype);
+Admin.prototype.constructor = Admin;
+
+
 let admin = new Admin('charles', 2345);
+console.log(admin.viewRooms());
 console.log(DB)
