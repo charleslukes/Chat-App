9824ed80e57e3e190b87636b75f0b1a902f7b7f7
Charles Chiakwa <charleslukes28@gmail.com>
2019-04-26 15:46:59 +0100

    includes all test cases for user file

--- user.test.js
+++ user.test.js
@@ -2,6 +2,7 @@ const User = require('./user');
 const DB = require('./DB');
 
 let user1 = new User('user1', 'user1@gmail.com', 1111);
+let user2 = new User('user2', 'user2@gmail.com', 2222);
 describe('User constructor ', () => {
     it('Should be defined', () => {
         expect(user1).toBeDefined();
@@ -25,5 +26,25 @@ describe('User constructor ', () => {
         DB.rooms.teens[0].roomCount = 5;
         expect(user1.joinRoom('teens')).toBe('Room is full, please join another')
     });
+    it('Should send message to room', () => {
+        expect(user1.sendChatMessage('teensde', 'Hey guys')).toBe('Room doesn\'t exists');
+        expect(user1.sendChatMessage('teens', 'Hey guys')).toBe('message delivered')
+        expect(DB.rooms.teens[2].forum).toContain('user1 just joined')
+    });
+    it('Should not give chat access to non members', () => {
+        expect(user1.sendChatMessage('adults')).toBe('You\'re not a member of this room, please join room.');
+    });
+    it('Should be able to leave chat room', () => {
+        expect(user1.leaveRoom('cmrjlf')).toBe('Room doesn\'t exists');
+        expect(user1.leaveRoom('teens')).toBe('You left the chat')
+        expect(user2.leaveRoom('teens')).toBe('You don\'t have access to this room')
+        expect(DB.rooms.teens[0].roomCount).toBe(4)
+        expect(DB.rooms.teens[1].members.length).toBe(0)
+        expect(DB.rooms.teens[2].forum).toContain('user1 left')
+    });
+    it('Should send reports to admin', () => {
+        expect(user1.sendReport('Need some Info')).toBe('Message delivered')
+        expect(DB.user_reports).toContain('Need some Info')
+    })
 })
 
