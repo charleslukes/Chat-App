caf76a75bb52ab5253426824703f9fdecef81a1e
Charles Chiakwa <charleslukes28@gmail.com>
2019-04-26 13:12:30 +0100

    include index file to run all text

---
+++ index.js
@@ -0,0 +1,13 @@
+const User = require('./user');
+const DB = require('./DB')
+const Admin = require('./Admin');
+
+
+let user1 = new User('mickey', 'mick@gmail.com', 123);
+let user2 = new User('spartan','spar@gmail.com', 222);
+let user3 = new User('amak','amak@gmail.com', 111);
+let user4 = new User('josmak','josk@gmail.com', 333);
+let user5 = new User('marcus','marcus@gmail.com', 444);
+let user6 = new User('piro','piro@gmail.com', 456);
+
+console.log(DB);
